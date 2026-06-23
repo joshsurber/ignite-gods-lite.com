cd908c7f38adc708b2bddd6bad69f1055481deac
Josh Surber <josh@surber.us>
2026-06-23 01:33:59 -0500

    chore: force integrate cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare'; // 1. Import the adapter
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // 2. Tell Astro to render on demand
+  adapter: cloudflare(), // 3. Hook up the adapter
+});
