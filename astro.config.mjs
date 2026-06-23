286a79d5a9710c3c71ae5a17cc99e7b9bf0c6f9d
Josh Surber <josh@surber.us>
2026-06-23 01:50:38 -0500

    fix: pivot to modern asset bindings to clear validation bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,5 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    // Change the asset binding name to prevent the Cloudflare conflict
-    assets: {
-      binding: 'LOCAL_ASSETS',
-    },
-  }),
+  adapter: cloudflare(),
 });
